1568c709f625c259029ba778c8ba03baa556373f
cj-mm <cmmarasigan@up.edu.ph>
2024-04-23 20:22:28 +0800

    discard window popup

--- src/scripts/context-menu/index.tsx
+++ src/scripts/context-menu/index.tsx
@@ -1,21 +1,21 @@
-import '@/styles/index.css'
-import { createRoot } from 'react-dom/client'
-import WindowPopup from './WindowPopup'
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { persistor, store } from '../../redux/store.js'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
+// import '@/styles/index.css'
+// import { createRoot } from 'react-dom/client'
+// import WindowPopup from './WindowPopup'
+// import React from 'react'
+// import { BrowserRouter } from 'react-router-dom'
+// import { persistor, store } from '../../redux/store.js'
+// import { Provider } from 'react-redux'
+// import { PersistGate } from 'redux-persist/integration/react'
 
-const container = document.getElementById('window-popup-root')
-const root = createRoot(container!)
+// const container = document.getElementById('window-popup-root')
+// const root = createRoot(container!)
 
-root.render(
-    <PersistGate persistor={persistor}>
-        <Provider store={store}>
-            <BrowserRouter>
-                <WindowPopup />
-            </BrowserRouter>
-        </Provider>
-    </PersistGate>
-)
+// root.render(
+//     <PersistGate persistor={persistor}>
+//         <Provider store={store}>
+//             <BrowserRouter>
+//                 <WindowPopup />
+//             </BrowserRouter>
+//         </Provider>
+//     </PersistGate>
+// )
