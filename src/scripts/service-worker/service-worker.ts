935b696975dc05f427e69f73e46518ca160c9c29
cj-mm <cmmarasigan@up.edu.ph>
2024-04-24 23:54:01 +0800

    remove unnecessary code

--- src/scripts/service-worker/service-worker.ts
+++ src/scripts/service-worker/service-worker.ts
@@ -1,4 +1,3 @@
-// import { handleRecord, fetchUser, fetchHandleLike, fetchHandleSave } from './api-req-handler'
 console.log('Background Service Worker Loaded')
 const windowPopupRoute = 'http://localhost:5173/window-popup?selectedText='
 
@@ -40,61 +39,12 @@ chrome.action.onClicked.addListener(() => {
 })
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    const { command, body } = message
+    const { command } = message
     switch (command) {
         case 'hello-world':
             console.log('Hello World, from the Background Service Worker')
             sendResponse({ success: true, message: 'Hello World' })
             break
-        // case 'record-counterargs':
-        //     const record = async () => {
-        //         const recorded = await handleRecord(
-        //             body.inputClaim,
-        //             body.summary,
-        //             body.body,
-        //             body.source
-        //         )
-        //         if (recorded) {
-        //             sendResponse({ success: true, data: recorded })
-        //         } else {
-        //             sendResponse({ success: false, data: 'Failed request' })
-        //         }
-        //     }
-        //     record()
-        //     return true
-        // case 'get-user':
-        //     const getUser = async () => {
-        //         const user = await fetchUser()
-        //         if (user) {
-        //             sendResponse({ success: true, data: user })
-        //         } else {
-        //             sendResponse({ success: false, data: 'Failed request' })
-        //         }
-        //     }
-        //     getUser()
-        //     return true
-        // case 'handle-like':
-        //     const handleLike = async () => {
-        //         const likeRes = await fetchHandleLike(body)
-        //         if (likeRes) {
-        //             sendResponse({ success: true, data: likeRes })
-        //         } else {
-        //             sendResponse({ success: false, data: 'Failed request' })
-        //         }
-        //     }
-        //     handleLike()
-        //     return true
-        // case 'handle-save':
-        //     const handleSave = async () => {
-        //         const saveRes = await fetchHandleSave(body)
-        //         if (saveRes) {
-        //             sendResponse({ success: true, data: saveRes })
-        //         } else {
-        //             sendResponse({ success: false, data: 'Failed request' })
-        //         }
-        //     }
-        //     handleSave()
-        //     return true
         default:
             console.log('default')
             break
